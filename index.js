b882426dec6806856fe654b271ed54e64a5b84fa
Olena Vdovych <olena.vdovych2@gmail.com>
2023-03-16 14:53:44 +0200

    Added class greeting and functionality for changed text

--- index.js
+++ index.js
@@ -122,15 +122,26 @@
         const now = new Date();
         const hour = now.getHours();
 
+        let text = '';
+
         if (hour > 5 && hour < 11) {
             body.style.backgroundImage = 'url(images/morning.jpg)';
+            text = 'Good morning!';
         } else if (hour >= 11 && hour < 17) {
             body.style.backgroundImage = 'url(images/day.jpg)';
+            text = 'Good day!';
         } else if (hour >= 17 && hour < 23 ) {
             body.style.backgroundImage = 'url(images/sunset.jpg)';
+            text = 'Good evening!';
         } else {
             body.style.backgroundImage = 'url(images/night.jpg)';
+            text = 'Good night!';
         }
+
+        const textElement = document.createElement('div');
+        textElement.textContent = text;
+        textElement.classList.add('greeting');
+        body.appendChild(textElement);
     };
     changeBackground();
 })();
